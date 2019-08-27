dee6d1ff6a02453191c1fd6925f91e8e8ed71feb
B533P-23\533 <djohnat_13@hotmail.com>
2019-08-27 11:48:40 -0400

    Movement de base

---
+++ LegendaryProject/Assets/movementplayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementplayer : MonoBehaviour {
+    private CharacterController characterControleur;
+    
+	// Use this for initialization
+	void Start () {
+        characterControleur = GetComponent<CharacterController>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        processMovementInput();
+
+    }
+    void processMovementInput()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontal,0,vertical);
+        Vector3 ajustedMovement = transform.TransformDirection(movement);
+        characterControleur.SimpleMove(ajustedMovement * Time.deltaTime * 500);
+    }
+}
