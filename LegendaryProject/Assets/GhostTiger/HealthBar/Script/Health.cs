6a2d09db45ed8ea547ed231b35649585e6f18722
Alexandre Reny <areny1998@hotmail.com>
2019-09-10 09:54:57 -0400

    Commit GhostTiger et HealthBar

---
+++ LegendaryProject/Assets/GhostTiger/HealthBar/Script/Health.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+    [SerializeField]
+    private int maxHealth = 100;
+
+    private int currentHealth;
+
+    public event Action<float> OnHealthPctChanged = delegate { };
+
+    private void OnEnable(){
+        currentHealth = maxHealth;
+    }
+
+    public void ModifiyHealth(int amount){
+        currentHealth += amount;
+
+        float currentHealthPct = (float)currentHealth / (float)maxHealth;
+        OnHealthPctChanged(currentHealthPct);
+    }
+
+    private void Update(){
+        if (Input.GetKeyDown(KeyCode.Space))
+            ModifiyHealth(-10);
+    }
+}
