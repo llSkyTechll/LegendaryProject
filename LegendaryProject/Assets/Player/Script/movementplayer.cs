4a3c93ebedb735ef78b21033133e3e2c59725a7c
Pier-Luc Nadeau <titi677pier@gmail.com>
2019-09-23 17:24:55 -0400

    Mouvement de la camera + debut du saut

--- LegendaryProject/Assets/Player/Script/movementplayer.cs
+++ LegendaryProject/Assets/Player/Script/movementplayer.cs
@@ -4,32 +4,41 @@ using UnityEngine;
 
 public class movementplayer : MonoBehaviour {
     private CharacterController characterControleur;
-    
-	// Use this for initialization
-	void Start () {
+    private Rigidbody rdb;
+    private float currentX = 0.0f;
+    private float momentum = 0.0f;
+    // Use this for initialization
+    void Start () {
         characterControleur = GetComponent<CharacterController>();
-
+        rdb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        //if(Input.GetAxis("Mouse X") != 0)
-        //{
-        //    print(Input.GetAxis("Mouse X"));
-        //}
-        //if (Input.GetAxis("Mouse Y") != 0)
-        //{
-        //    print(Input.GetAxis("Mouse Y"));
-        //}
+        if (Input.GetAxis("Jump") != 0)
+        {
+            momentum = 10;
+        }
+        currentX += Input.GetAxis("Mouse X");
         processMovementInput();
-
+      
     }
     void processMovementInput()
-    {
+    { 
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
+        if (momentum > 0)
+        {
+            transform.Translate(0, momentum/10, 0);
+            momentum--;
+        }
         Vector3 movement = new Vector3(horizontal,0,vertical);
         Vector3 ajustedMovement = transform.TransformDirection(movement);
         characterControleur.SimpleMove(ajustedMovement * Time.deltaTime * 500);
+        if (ajustedMovement!= new Vector3())
+        {
+            Quaternion rotation = Quaternion.Euler(0, currentX, 0);
+            transform.rotation = rotation;
+        }
     }
 }
