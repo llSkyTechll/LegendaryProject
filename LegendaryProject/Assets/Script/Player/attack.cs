f9f77992bedc3ddc7e7baa3d09bd5080b46c9709
Alexandre Reny <areny1998@hotmail.com>
2019-10-07 16:56:06 -0400

    weapon + equipment + objet + armor

--- LegendaryProject/Assets/Script/Player/attack.cs
+++ LegendaryProject/Assets/Script/Player/attack.cs
@@ -9,21 +9,30 @@ public class attack : MonoBehaviour {
     private bool axisInUse = false;
     private Animator animator;
     private CharacterController characterController;
+    private Weapon equippedWeapon;
+    private float attackCooldown = 0f;
+
     // Use this for initialization
     void Start () {
         animator = GetComponentInChildren<Animator>();
         characterController = GetComponent<CharacterController>();
+        equippedWeapon = GetComponentInChildren<Weapon>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        attackCooldown -= Time.deltaTime;
         if (Input.GetAxis("Fire1")!=0)
         {
             if (axisInUse == false && characterController.isGrounded)
             {
-                axisInUse = true;
-                animator.SetTrigger("Attacking");
-                RaycastSingle();               
+                if (attackCooldown <= 0f)
+                {
+                    axisInUse = true;
+                    animator.SetTrigger("Attacking");
+                    RaycastSingle();
+                    attackCooldown = 1.5f;
+                } 
             }
         }
         if (Input.GetAxis("Fire1") == 0)
@@ -42,13 +51,14 @@ public class attack : MonoBehaviour {
         RaycastHit hitinfo ;
         if (Physics.Raycast(origin,direction,out hitinfo,MaxDistance,layerMask, QueryTriggerInteraction.UseGlobal))
         {
-            DealRandomDamage(10, 40,hitinfo);
+            DealRandomDamage(equippedWeapon.minDamage, equippedWeapon.maxDamage,hitinfo);
         }
     }
 
     private void DealRandomDamage(int lowStat,int highStat, RaycastHit hitinfo)
     {
         System.Random rnd = new System.Random();
-        hitinfo.collider.gameObject.GetComponent<Health>().TakeDamage(rnd.Next(lowStat, highStat+1));
+        int damage = rnd.Next(lowStat, highStat + 1);
+        hitinfo.collider.gameObject.GetComponent<Health>().TakeDamage(damage);
     }
 }
