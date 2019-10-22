8d47a838e56110d6bf9a46c420c982b995e19e92
Zachary Alexandre <zachary.alexandre430@gmail.com>
2019-10-22 11:19:08 -0400

    Ajouter footsteps

--- LegendaryProject/Assets/Script/Player/Player.cs
+++ LegendaryProject/Assets/Script/Player/Player.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class Player : Character {
     //GameObject player;
     public float speed = 1;
+    CharacterController cc;
     //Rigidbody rbd;
     //AudioSource audioSource;
     //private Animator animator;
@@ -16,6 +17,8 @@ public class Player : Character {
         //audioSource = GameObject.FindGameObjectWithTag("SoundPlayer").GetComponent<AudioSource>();
         SetLife(100);
         health = GetComponent<Health>();
+        cc = GetComponent<CharacterController>();
+        soundplayer = GetComponent<AudioSource>();
         //animator = GetComponent<Animator>();
     }
 
@@ -25,6 +28,7 @@ public class Player : Character {
         {
             gameObject.GetComponent<Character>().Die();
         }
+        Footsteps();
     }
 
     public override void Die()
@@ -40,6 +44,19 @@ public class Player : Character {
 
     public override void Footsteps()
     {
-        
+        if(cc.velocity.magnitude > 0.5f && cc.velocity.magnitude < 5f && soundplayer.isPlaying == false && GetComponent<movementplayer>().isGrounded)
+        {
+            soundplayer.clip = step;
+            soundplayer.pitch = Random.Range(0.8f, 1);
+            soundplayer.volume = Random.Range(0.8f, 1.1f);
+            soundplayer.Play();
+        }
+        else if(soundplayer.isPlaying == false && cc.velocity.magnitude > 5f && GetComponent<movementplayer>().isGrounded)
+        {
+            soundplayer.clip = step;
+            soundplayer.pitch = Random.Range(1.3f, 1.5f);
+            soundplayer.volume = Random.Range(0.8f, 1.1f);
+            soundplayer.Play();
+        }
     }
 }
