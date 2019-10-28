7ee645e4a448a4c6ac471b4179dd0f3078e750b9
Alexandre Reny <areny1998@hotmail.com>
2019-10-28 15:47:58 -0400

    Boss almost done reste la détection de lave

---
+++ LegendaryProject/Assets/LavaDetection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LavaDetection : MonoBehaviour {
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //SceneManager.LoadScene("Boss Room");
+        }
+    }
+}
