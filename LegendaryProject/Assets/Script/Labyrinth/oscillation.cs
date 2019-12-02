4f02b9b06fe385f73a21479ea3e9e0a89f4df7f7
Johnatan <djohnat_@hotmail.com>
2019-12-02 17:14:50 -0500

    Door up and Down plus monster

--- LegendaryProject/Assets/Script/Labyrinth/oscillation.cs
+++ LegendaryProject/Assets/Script/Labyrinth/oscillation.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class oscillation : MonoBehaviour {
+[DisallowMultipleComponent]
+public class Oscillation : MonoBehaviour
+{
+    [SerializeField] Vector3 mouvementVector = new Vector3(0, 5f, 0);
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Range(0, 1)]
+    [SerializeField]
+    Vector3 startingPos;
+
+
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    public void DoorUp()
+    {
+        Vector3 offset = mouvementVector;
+        transform.position = startingPos + offset;
+    }
+    public void DoorDown()
+    {
+        Vector3 offset = mouvementVector;
+        transform.position = startingPos - offset;
+    }
 }
